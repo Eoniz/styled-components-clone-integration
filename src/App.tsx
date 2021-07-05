27c22a2fd1a6955a0fba67a8d5c854b68eac163a
Eoniz <nathan.artisien@gmaiL.com>
2021-07-05 22:42:50 +0200

    update: README

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from './styled';
+
+const spacing = "16px";
+
+const Heading = styled.h1`
+  fontSize: 4rem;
+  color: white;
+  margin: 0;
+`;
+
+const Wrapper = styled.div`
+  position: absolute;
+  left: 0;
+  padding: ${spacing};
+  right: 0;
+  bottom: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  backgroundColor: #432432;
+  margin: 0;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Heading>
+        Hello World !
+      </Heading>
+    </Wrapper>
   );
 }
 
---
+++ src/styled/index.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+
+interface IHOCProps {
+    type: keyof React.ReactHTML;
+    style: React.CSSProperties;
+}
+
+const HOC = (props: IHOCProps) => {
+    return (childProps: any) => {
+        const { children, ...rest } = childProps;
+        return React.createElement(
+            props.type,
+            {
+                style: props.style,
+                ...rest
+            },
+            children
+        );
+    }
+};
+
+const reg = /(\w+):\s*(.+)/;
+
+const constructStringLitteral = (props: TemplateStringsArray, ...rest: string[]) => {
+    let r = "";
+    props.forEach((value, i) => {
+        r += value + (i < rest.length ? rest[i] : "");
+    });
+
+    return r;
+}
+
+const extractCSSProperties = (props: TemplateStringsArray, ...rest: string[]) => {
+    const styleLitteral = (
+        constructStringLitteral(props, ...rest)
+            .replaceAll('\n', '')
+            .split(';')
+            .map((a) => a.trim())
+            .filter((a) => a.length > 0)
+    );
+
+    const styleProps: Record<string, string> = {};
+
+    for (const property of styleLitteral) {
+        const results = property.match(reg);
+        if (results === null) {
+            continue;
+        }
+        if (results[1] === null || results[2] === null) {
+            continue;
+        }
+
+        styleProps[results[1]] = results[2];
+    }
+
+    return styleProps as React.CSSProperties;
+}
+
+const styled = {
+    h1: (props: TemplateStringsArray, ...rest: string[]) => {
+        return HOC({type: "h1", style: extractCSSProperties(props, ...rest)});
+    },
+    h2: (props: TemplateStringsArray, ...rest: string[]) => {
+        return HOC({type: "h2", style: extractCSSProperties(props, ...rest)});
+    },
+    h3: (props: TemplateStringsArray, ...rest: string[]) => {
+        return HOC({type: "h3", style: extractCSSProperties(props, ...rest)});
+    },
+    h4: (props: TemplateStringsArray, ...rest: string[]) => {
+        return HOC({type: "h4", style: extractCSSProperties(props, ...rest)});
+    },
+    h5: (props: TemplateStringsArray, ...rest: string[]) => {
+        return HOC({type: "h5", style: extractCSSProperties(props, ...rest)});
+    },
+    h6: (props: TemplateStringsArray, ...rest: string[]) => {
+        return HOC({type: "h6", style: extractCSSProperties(props, ...rest)});
+    },
+    input: (props: TemplateStringsArray, ...rest: string[]) => {
+        return HOC({type: "input", style: extractCSSProperties(props, ...rest)});
+    },
+    div: (props: TemplateStringsArray, ...rest: string[]) => {
+        return HOC({type: "div", style: extractCSSProperties(props, ...rest)});
+    },
+    form: (props: TemplateStringsArray, ...rest: string[]) => {
+        return HOC({type: "form", style: extractCSSProperties(props, ...rest)});
+    },
+};
+
+export default styled;
